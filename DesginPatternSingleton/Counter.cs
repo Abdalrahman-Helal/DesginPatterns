4b932dbd112b1cfd4a39a269fac8f2764198f874
Abdlrhmnhelal@gmail.com <Abdlrhmnhelal@gmail.com>
2025-02-18 21:47:49 +0200

    Added Desgin Pattern Signleton

---
+++ DesginPatternSingleton/Counter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesginPatternSingleton
+{
+    class Counter
+    {
+        public int count = 0;
+        private static Counter Instance = new Counter();
+        private Counter()
+        {
+
+        }
+
+        public static Counter GetInstance()
+        {
+            return Instance;
+        }
+
+        public void AddOne()
+        {
+            count++;
+        }
+    }
+}
---
+++ DesginPatternSingleton/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DesginPatternSingleton
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter C1 = Counter.GetInstance();
+            Counter C2 = Counter.GetInstance();
+
+            C1.AddOne();
+            Console.WriteLine("Counter 1 = " + C1.count.ToString());
+
+            C2.AddOne();
+            Console.WriteLine("Counter 2 = " + C2.count.ToString());
+
+            Console.WriteLine();
+
+            C1.AddOne();
+            Console.WriteLine("Counter 1 = " + C1.count.ToString());
+            Console.WriteLine("Counter 2 = " + C2.count.ToString());
+            Console.ReadKey();
+
+
+        }
+    }
+}
